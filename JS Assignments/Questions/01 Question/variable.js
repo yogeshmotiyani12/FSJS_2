650dce4291db31138ef62a0c7bc089a5b0dbe203
yogeshmotiyani12 <yogeshmotiyani12@gmail.com>
2023-01-18 23:59:38 +0530

    JS practice questions.

---
+++ JS Assignments/Questions/01 Question/variable.js
@@ -0,0 +1,14 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+const name = "Yogesh" 
+
+const isWorkingProfessional = true
+
+const havedetails = null
+
+const willgetdeta = undefined
+
+console.log("The data type of" + name + "is" + typeof name);
+console.log("The data type of" + isWorkingProfessional + "is" + typeof isWorkingProfessional);
+onsole.log("The data type of" + havedetails + "is" + typeof havedetails);
+console.log("The data type of" + willgetdeta + "is" + typeof willgetdeta);
