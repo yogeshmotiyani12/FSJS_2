256926ede6ba614c27fd74ab4de414b95e1cf625
yogeshmotiyani12 <yogeshmotiyani12@gmail.com>
2023-02-03 23:59:54 +0530

    Added 25 solution.

--- JS Assignments/Questions/25 Question/script.js
+++ JS Assignments/Questions/25 Question/script.js
@@ -8,5 +8,20 @@
 //     - Obese: BMI is 30 or more
 
 
-let userHeight = prompt("Enter your height(in cm)");
-console.log(userHeight);
+function calcBMI (height, weight){
+    let check_bmi = (weight/(height*height/10000))
+    console.log(`BMI is ${check_bmi}`);
+    if (check_bmi < 18.5){
+        console.log("Your are underweight as per the BMI.");
+    }else if(check_bmi >= 18.5 && check_bmi <= 24.9){
+        console.log("Normal weight as per the BMI");
+    }else if(check_bmi >= 25 && check_bmi <= 29.9){
+        console.log("Overweight as per the BMI.");
+    }else if(check_bmi > 30){
+        console.log("Obese as per the BMI.");
+    }
+}
+
+const Pheight = 40;
+const Pweight = 1.5;
+calcBMI(Pheight, Pweight);
