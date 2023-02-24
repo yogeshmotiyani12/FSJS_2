c7334a25dfa1bb9091d6524698dcb1abf6e880cd
yogeshmotiyani12 <yogeshmotiyani12@gmail.com>
2023-02-24 23:59:52 +0530

    Added Objects practice file.

---
+++ Practice_work/objects.js
@@ -0,0 +1,15 @@
+
+const Obj1 = {};  // An empty object
+
+console.log(Obj1);  // {}
+
+
+const Obj2 = {
+    name: 'Jack',
+    age: 22
+}
+ 
+console.log(Obj2);   // { name: 'Jack', age: 22 }
+console.log(Obj2.name); // Jack
+console.log(Obj2.age); // 22
+
