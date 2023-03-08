ecf58dbabdd3ae46e72b6c36ae1a261ff69addb7
yogeshmotiyani12 <yogeshmotiyani12@gmail.com>
2023-03-08 22:01:46 +0530

    Removed React JS 04-05 folder due to .gitmodule issues

--- React JS/04 - 05  march/07_react_router_dom/07_react_router_dom/src/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => {
-  return <h1>About us page</h1>;
-};
-
-export default About;
--- React JS/04 - 05  march/07_react_router_dom/07_react_router_dom/src/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Contact = () => {
-  return (
-    <h3 className='text'>Contact us Page</h3>
-  )
-}
-
-export default Contact
