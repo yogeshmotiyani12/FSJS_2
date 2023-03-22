c4374a218a91aae53d6912ddf215b6df9f9c14ce
yogeshmotiyani12 <yogeshmotiyani12@gmail.com>
2023-03-22 23:15:05 +0530

    removed the folder due to hosting issues on netlfiy

--- React JS/11 - 12 March/12 March redux-todo/redux-todo/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import todoReducer from '../features/todoSlice'
-
-export const store = configureStore({
-    reducer: {
-        todos:todoReducer
-    }
-})
